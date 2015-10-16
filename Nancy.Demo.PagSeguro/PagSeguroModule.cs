9597db01576f8399baf20aae89d38281a5e25333
fagnercarvalho <fagnerncarvalho@gmail.com>
2015-10-16 05:07:45 -0300

    Initial commit

---
+++ Nancy.Demo.PagSeguro/PagSeguroModule.cs
@@ -0,0 +1,41 @@
+﻿namespace Nancy.Demo.PagSeguro
+{
+    using System.Configuration;
+    using System.Net.Http;
+    using System.Text;
+    using System.Xml.Linq;
+
+    public class PagSeguroModule : NancyModule
+    {
+        public PagSeguroModule()
+        {
+            Get["/payment", true] = async (parameters, ct) =>
+            {
+                var httpClient = new HttpClient();
+
+                var xml = XDocument.Load("payment.xml").ToString();
+                var content = new StringContent(
+                    xml,
+                    Encoding.GetEncoding("ISO-8859-1"),
+                    "application/xml");
+
+                var email = ConfigurationManager.AppSettings["EMAIL"];
+                var token = ConfigurationManager.AppSettings["TOKEN"];
+
+                var response = await httpClient.PostAsync($@"https://ws.sandbox.pagseguro.uol.com.br/v2/checkout?email={email}&token={token}", content);
+
+                string code = string.Empty;
+                if (response.IsSuccessStatusCode)
+                {
+                    var xmlContent = await response.Content.ReadAsStreamAsync();
+                    code = XDocument.Load(xmlContent).Root.Element("code").Value;
+
+                    return Response.AsRedirect($@"https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?code={code}");
+                } else
+                {
+                    return Response.AsJson(response.ReasonPhrase, HttpStatusCode.InternalServerError);
+                }
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+Nancy.Demo.PagSeguro
+===
+
+A demo for a simple Nancy + PagSeguro API integration.
+
+Requirements
+---
+
+- Visual Studio 2015
+- .NET Framework 4.6
+
+Install instructions
+---
+
+- Clone this repository;
+- Set your own email and token (create a new account [here](https://pagseguro.uol.com.br/registration/registration.jhtml?tipo=cadastro&sandbox=&ep=13#!vendedor));
+- Edit `payment.xml` to add your own payment data.
+- Run the project.
+
+Usage
+---
+Just access http://localhost:3579/payment.
+
+Missing features
+---
+
+- Transparent checkout;
+- Billing plans;
+- [And more](https://pagseguro.uol.com.br/para_seu_negocio/venda_pela_internet.jhtml).
